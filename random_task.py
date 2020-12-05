bfe18330a6df80786481ff3246fcfb3fd5164543
Dawid Płocki <dawid.plocki@gmail.com>
2020-12-05 15:26:24 +0100

    Implement random_task.py

---
+++ random_task.py
@@ -0,0 +1,17 @@
+import random
+from todoist.api import TodoistAPI
+import config
+
+api = TodoistAPI(config.todoist_api_key)
+api.sync()
+
+project = next((p for p in api.state['projects'] if p['name'] == 'Może kiedyś'))
+project_id = project['id']
+
+project_items = list(
+        item
+        for item in api.items.state['items']
+        if item['project_id'] == project_id and item['parent_id'] == None
+    )
+
+print(random.choice(project_items).data['content'])
