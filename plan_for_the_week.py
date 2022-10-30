b68d728ef745dc40a59dd72f8ed10e67c0cea4f8
Dawid Plocki <dawid.plocki@gmail.com>
2022-10-30 11:41:44 +0100

    Black format

--- plan_for_the_week.py
+++ plan_for_the_week.py
@@ -2,22 +2,31 @@ import random
 from todoist_api_python.api import TodoistAPI
 import config
 
+
 def generate():
     maximum = 5
-    distribute_table = [i for multiplayer in range(1, maximum)
-            for i in [multiplayer] * (maximum - multiplayer)]
+    distribute_table = [
+        i
+        for multiplayer in range(1, maximum)
+        for i in [multiplayer] * (maximum - multiplayer)
+    ]
 
     while True:
-        yield distribute_table[random.randint(0, 9)] 
+        yield distribute_table[random.randint(0, 9)]
+
 
 api = TodoistAPI(config.todoist_api_key)
 
-project = next(p for p in api.get_projects() if p.name == 'Może kiedyś')
+project = next(p for p in api.get_projects() if p.name == "Może kiedyś")
 project_id = project.id
 
 pass_in_next_day = False
 for number_of_elements, number_of_day in zip(generate(), range(7)):
     if not pass_in_next_day:
-        api.add_task(content=f'Poświęć mroku {number_of_elements}', project_id=project_id, due_string=f'+{number_of_day + 1} day')
+        api.add_task(
+            content=f"Poświęć mroku {number_of_elements}",
+            project_id=project_id,
+            due_string=f"+{number_of_day + 1} day",
+        )
 
     pass_in_next_day = number_of_elements >= 3
--- random_task.py
+++ random_task.py
@@ -5,13 +5,15 @@ import config
 api = TodoistAPI(config.todoist_api_key)
 api.sync()
 
-project = next((p for p in api.state['projects'] if p['name'] == 'Może kiedyś'))
-project_id = project['id']
+project = next((p for p in api.state["projects"] if p["name"] == "Może kiedyś"))
+project_id = project["id"]
 
 project_items = list(
-        item
-        for item in api.items.state['items']
-        if item['project_id'] == project_id and item['parent_id'] == None and item['in_history'] == 0
-    )
+    item
+    for item in api.items.state["items"]
+    if item["project_id"] == project_id
+    and item["parent_id"] == None
+    and item["in_history"] == 0
+)
 
-print(random.choice(project_items).data['content'])
+print(random.choice(project_items).data["content"])
