8addd3c422acd77e0d36bb8dcd6ca8f6dbef3253
Dawid Plocki <dawid.plocki@gmail.com>
2022-10-30 12:44:51 +0100

    Adjust 'random task' tool

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
-from flask import Flask
+from flask import url_for, Flask
+from todoist_api_python.api import TodoistAPI
+import config
+
 
 app = Flask(__name__)
+api = TodoistAPI(config.todoist_api_key)
+
+
+@app.route("/random_task")
+def random_task():
+    from tools.random_task import run
+
+    result = run(api, "Może kiedyś")
+    return f"<p>A random task from given project: '{result.content}'</p>"
+
+
+@app.route("/plan_for_the_week")
+def plan_for_the_week():
+    return "plan_for_the_week"
 
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return f"""<!doctype html>
+<body>
+    <h1>Tool for Todoist</h1>
+
+    <ul>
+        <li><a href="{url_for('random_task')}">Random task</a></li>
+        <li><a href="{url_for('plan_for_the_week')}">Plan for the week</a></li>
+    </ul>
+</body>
+"""
 
 
 if __name__ == "__main__":
---
+++ tools/random_task.py
@@ -0,0 +1,15 @@
+import random
+
+def run(api, project_name):
+    project = next(p for p in api.get_projects() if p.name == project_name)
+    project_id = project.id
+
+    project_items = list(
+        item
+        for item in api.get_tasks()
+        if item.project_id == project_id
+        and item.parent_id == None
+        and item.completed == False
+    )
+
+    return random.choice(project_items)
