8cf33bf14b199be659d69bb22474135a90fabe03
Dawid Plocki <dawid.plocki@gmail.com>
2023-07-15 20:36:09 +0200

    Reproduce by forgetting curve: Add posibility to reproduce labels of base task

--- tools/reproduce_by_forgetting_curve.py
+++ tools/reproduce_by_forgetting_curve.py
@@ -11,13 +11,14 @@ def iterval_generator():
         move += 1
 
 
-def generate_task_for_given(api, task, how_many_repeats: int):
+def generate_task_for_given(api, task, label_name: str, how_many_repeats: int):
     for iterval in islice(iterval_generator(), how_many_repeats):
         api.add_task(
             content=task.content,
             project_id=task.project_id,
             description=task.description,
             due_string=f"+{iterval} day",
+            labels=remove_label_from_task(task, label_name)
         )
 
 
@@ -27,16 +28,20 @@ def get_all_task_for_given_label(api, label_name: str):
     if label_name in item.labels)
 
 
-def remove_label_from_task(api, task):
+def close_task(api, task):
     return api.close_task(task_id=task.id)
+
+
+def remove_label_from_task(task, label_name):
+    return [label for label in task.labels if label != label_name]
 
 
 def run(api, label_name: str, how_many_repeats) -> int:
     count = 0
 
     for task in get_all_task_for_given_label(api, label_name):
-        generate_task_for_given(api, task, how_many_repeats)
-        remove_label_from_task(api, task)
+        generate_task_for_given(api, task, label_name, how_many_repeats)
+        close_task(api, task)
         count += 1
 
     return count
